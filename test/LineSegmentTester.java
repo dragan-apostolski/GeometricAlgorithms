f62b7bd58823de2f50ac61607fa8471cc9ef47ea
drapostolski <drapostolski@gmail.com>
2017-05-04 11:37:28 +0200

    Add juint tests

--- test/LineSegmentTester.java
+++ test/LineSegmentTester.java
@@ -5,25 +5,93 @@ import GeometricAlgorithms.Point;
 import GeometricAlgorithms.Segment;
 import GeometricAlgorithms.Side;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
 
 public class LineSegmentTester {
 
+
+    @Test
+    public static void anySegmentsIntersectTest(){
+        /*
+
+        6 |     f
+        5 | a
+        4 |          d
+        3 | c
+        2 |         b
+        1 |   e
+        0 -------------
+          0 1 2 3 4 5 6
+         */
+
+        Point a = new Point(1, 5, Side.LEFT);
+        Point b = new Point(5, 2, Side.RIGHT);
+        Point c = new Point(1, 3, Side.LEFT);
+        Point d = new Point(5.5, 4, Side.RIGHT);
+        Point e = new Point(1.9, 1, Side.LEFT);
+        Point f = new Point(3, 6, Side.RIGHT);
+        List<Segment> segments = Arrays.asList(new Segment(a, b), new Segment(c, d), new Segment(e, f));
+
+        Set<Segment> segmentSet = new HashSet<>(segments);
+
+        Assert.assertThat(LineSegments.anySegmentIntersects(segmentSet), is(true));
+    }
+
+    @Test
+    public static void segmentsIntersectTest(){
+
+
+         /*
+
+        6 |         f
+          |
+        5 | e
+          |
+        4 |
+          |
+        3 | a
+          |              d
+        2 |           b
+          | c
+        1 |
+        0 -------------------
+          0  1  2  3  4  5  6
+         */
+
+
+        Point a = new Point(.7, 3, Side.LEFT);
+        Point b = new Point(4, 2, Side.RIGHT);
+        Point c = new Point(0.5, 1.5, Side.LEFT);
+        Point d = new Point(5, 2.5, Side.RIGHT);
+
+        Point e = new Point(.7, 5, Side.LEFT);
+        Point f = new Point(3.5, 6, Side.RIGHT);
+
+        Segment ab = new Segment(a, b);
+        Segment cd = new Segment(c, d);
+        Segment ef = new Segment(e, f);
+
+        Segment ad = new Segment(a, d);
+        Segment cb = new Segment(c, b);
+
+        Segment cf = new Segment(c, f);
+        Segment eb = new Segment(e, b);
+
+        Assert.assertThat(LineSegments.segmentsIntersect(ab, cd), is(true));
+        Assert.assertThat(LineSegments.segmentsIntersect(ab, ef), is(false));
+        Assert.assertThat(LineSegments.segmentsIntersect(ad, cb), is(false));
+        Assert.assertThat(LineSegments.segmentsIntersect(cf, eb), is(true));
+    }
+
+
+
     public static void main(String[] args) {
-        Set<Segment> points = new HashSet<>();
-        Point p1 = new Point(.5, 3.7, Side.LEFT);
-        Point p2 = new Point(4, 1.7, Side.RIGHT);
-        Point p3 = new Point(.6, 1.3, Side.LEFT);
-        Point p4 = new Point(5, 1.3, Side.RIGHT);
-        Point p5 = new Point(2, .6, Side.LEFT);
-        Point p6 = new Point(3, 2, Side.RIGHT);
-
-        points.add(new Segment(p1, p2));
-        points.add(new Segment(p3, p4));
-        points.add(new Segment(p5, p6));
-
-        System.out.println(LineSegments.segmentsIntersect(new Segment(p1, p2), new Segment(p5, p6)));
-        System.out.println(LineSegments.anySegmentIntersects(points));
+        anySegmentsIntersectTest();
+        segmentsIntersectTest();
     }
 }
